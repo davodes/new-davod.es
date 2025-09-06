f8358fbe6a6742ecae46fb1773171a0af8a2b597
davod <davodhm@gmail.com>
2025-09-06 12:07:57 +0200

    Header and hero updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { literal } from 'astro:schema';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -16,6 +18,17 @@ export default {
           800: '#075985',
           900: '#0c4a6e',
           950: '#082f49',
+          // LILA,
+          // 100: '#f3e8ff',
+          // 200: '#e9d5ff',
+          // 300: '#d8b4fe',
+          // 400: '#c084fc',
+          // 500: '#a855f7',
+          // 600: '#9333ea',
+          // 700: '#7e22ce',
+          // 800: '#6b21a8',
+          // 900: '#581c87',
+          // 950: '#3b0764',
         },
         dark: {
           50: '#f8fafc',
